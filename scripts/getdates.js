22db8f7799906722dfc1940aff3383fd0c108062
MaCK34 <prom23hugomarcelo.sa@gmail.com>
2025-09-04 16:37:10 -0500

    added the current year and last modification in the .js file

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,5 @@
-<script>
-    document.getElementById("currentyear").textContent = new Date().getFullYear();
 
-    document.getElementById("lastModified").textContent =
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").textContent =
     "Last Modification: " + document.lastModified;
-</script>
