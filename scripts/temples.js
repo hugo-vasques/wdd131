d8608f8249fe795836df651f3ef656b335db0e1d
MaCK34 <prom23hugomarcelo.sa@gmail.com>
2025-09-11 16:51:04 -0500

    nav menu with the hamburguer button

---
+++ scripts/temples.js
@@ -0,0 +1,13 @@
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").textContent =
+    "Last Modification: " + document.lastModified;
+
+// Button type "Hamburguer" //
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
