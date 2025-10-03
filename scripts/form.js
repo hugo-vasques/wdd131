c8cf6f20150274dc64f95fcfed841940746b8eeb
MaCK34 <prom23hugomarcelo.sa@gmail.com>
2025-10-03 15:13:56 -0500

    added style to the textbox

--- scripts/form.js
+++ scripts/form.js
@@ -32,12 +32,3 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 document.getElementById("lastModified").textContent =
     "Last Modification: " + document.lastModified;
-
-// Button type "Hamburguer" //
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('nav');
-
-hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
-    hamButton.classList.toggle('open');
-});
